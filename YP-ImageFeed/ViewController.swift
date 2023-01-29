6e169601268030effc4d0a8567496cf5b845bd55
Bogdan Polygalov <bogdan.polygalov@gmail.com>
2023-01-29 15:23:18 +0500

    Initial Commit

---
+++ YP-ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YP-ImageFeed
+//
+//  Created by Богдан Полыгалов on 29.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
