36a353762efd399feeb44517e9cf8ee1fc09db30
Bogdan Polygalov <bogdan.polygalov@gmail.com>
2023-02-03 00:28:32 +0500

    Сверстал экран профиля пользователя и добавил для него контроллер

---
+++ YP-ImageFeed/ProfileViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileViewController.swift
+//  YP-ImageFeed
+//
+//  Created by Богдан Полыгалов on 02.02.2023.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet private weak var idLabel: UILabel!
+    @IBOutlet private weak var statusLabel: UILabel!
+    @IBOutlet private weak var exitButton: UIButton!
+    @IBOutlet private weak var userPhoto: UIImageView!
+    
+    @IBAction func tapExitButton(_ sender: UIButton) {
+        
+    }
+    
+}
